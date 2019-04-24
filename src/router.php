478a3354b7209fb332d6200bb4e4fcb262f3ad29
Loannt <nguyentloan13954@gmail.com>
2019-04-24 09:15:47 +0700

    mvc responsitory v1

---
+++ src/router.php
@@ -0,0 +1,28 @@
+<?php
+namespace MVC;
+
+class Router
+{
+
+    static public function parse($url, $request)
+    {
+        $url = trim($url);
+
+        if ($url == "/mvc/src/")
+        {
+            $request->controller = "tasks";
+            $request->action = "index";
+            $request->params = [];
+        }
+        else
+        {
+            $explode_url = explode('/', $url);
+            $explode_url = array_slice($explode_url, 2);
+            $request->controller = $explode_url[1];
+            $request->action = $explode_url[2];
+            $request->params = array_slice($explode_url, 3);
+        }
+
+    }
+}
+?>
